ac582bd973e70c2e8d3460527c38e2a8aacc7534
Amalie S <amaliesmidth@gmail.com>
2018-09-13 15:47:54 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,23 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    keys_array = []
+    arguments.each do |key, value|
+      if arguments.include?(value)
+        keys_array << key
+      end
+    end
+    keys_array
   end
 end
+
+# class Hash
+#   def keys_of(*arguments)
+#     keys_array = []
+#     arguments.each do |key, value|
+#       if arguments.include?(value)
+#         keys_array.push(key)
+#       end
+#     end
+#     keys_array
+#   end
+# end
